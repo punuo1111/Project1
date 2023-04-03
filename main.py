736c8aa837350d11b174f44520a4397e54b19a26
ponuo <ponuo0905@naver.com>
2023-04-03 15:09:55 +0900

    MyCommit1 더하기 기능을 완성함 ....

---
+++ main.py
@@ -0,0 +1,10 @@
+##메서드 선언부
+def add_func(n1, n2):
+    res = n1+n2
+    return res
+##전역 변수부(클래스 변수, 인스턴스변수)
+num1, num2, result = 100,200,0
+
+##메인 코드부
+result = add_func(num1, num2)
+print(num1, '+' , num2, '=', result)
