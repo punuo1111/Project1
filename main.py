4ba0b3518f749ef434c79dc2d335576921f378d0
ponuo <ponuo0905@naver.com>
2023-04-03 15:18:21 +0900

    MyCommit2 곱하기 함수 추가

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ def add_func(n1, n2):
 def sub_func(n1, n2):
     return n1-n2
 
+def sub2_func(n1, n2):
+    return n1*n2
+
+
 ##전역 변수부(클래스 변수, 인스턴스변수)
 num1, num2, result = 100,200,0
 
@@ -15,3 +19,6 @@ print(num1, '+' , num2, '=', result)
 
 result = sub_func(num1, num2)
 print(num1, '-' , num2, '=', result)
+
+result = sub2_func(num1, num2)
+print(num1, '*' , num2, '=', result)
